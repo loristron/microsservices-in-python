13a46ec1490f78574b761ee5a5987003e40363a2
loristron <loremmiranda@gmail.com>
2022-05-11 15:08:38 -0400

    Add route to render static template

--- src/app.py
+++ src/app.py
@@ -1,12 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+# DEFAULT FLASK APP PAGE
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
+# RETURNING JSONIFY CONTENT
 @app.route("/health", methods=['GET'])
 def health():
     
@@ -14,6 +15,12 @@ def health():
         status = "UP"
     )
 
+# STATIC HTML PAGE
+@app.route('/details', methods=['GET'])
+def details():
+    return render_template('index.html')
 
+# RUNNING WITH PYTHON3 APP.PY
+# CHOOSING HOST AND PORT 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
