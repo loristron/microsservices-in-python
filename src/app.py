20e4b5754f73ecd1996779a890b9aa7f620e9f2a
loristron <loremmiranda@gmail.com>
2022-05-11 15:02:39 -0400

    Jsonify first URL and also running with python3 instead of flask run (no need to export FLASK_APP=variable

--- src/app.py
+++ src/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/health", methods=['GET'])
+def health():
+    
+    return jsonify(
+        status = "UP"
+    )
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
